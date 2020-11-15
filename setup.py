b33fac4562356513f6614f36ab91d51d5970ac72
Rick Solis <rick.solis@gmail.com>
2020-11-14 23:27:12 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A linear regression analysis of red wine properties and alcohol content',
+    author='Rick Solis',
+    license='',
+)
